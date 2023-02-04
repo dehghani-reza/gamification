ba1745d533ea89a6b3111c4e42748fdf29651978
dehghani <dehghan7447@gmail.com>
2023-02-04 10:59:57 +0330

    chore(base-entity): complete class BaseEntity.java

--- src/main/java/ir/dehghani/gamification/entities/BaseEntity.java
+++ src/main/java/ir/dehghani/gamification/entities/BaseEntity.java
@@ -2,24 +2,23 @@ package ir.dehghani.gamification.entities;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
-@Getter@Setter@NoArgsConstructor
-public abstract class BaseEntity implements Serializable {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private Short version;
-    private LocalDateTime createdDate;
-    private LocalDateTime updatedDate;
+@Getter
+@Setter
+@NoArgsConstructor
+public abstract class BaseEntity extends MainBaseEntity {
+    private LocalDateTime createDate;
+    private Long createById;
+    private LocalDateTime updateDate;
+    private Long updateById;
+    private LocalDateTime deleteDate;
+    private Long deleteById;
+    private Boolean isDeleted;
+    private Integer rowLevelId;
+    private Integer priority;
 }
