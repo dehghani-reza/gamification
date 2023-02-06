9b22a91c314571bf981ae62c64e06454d777bcc1
Danial <dmehrabi73@gmail.com>
2023-02-06 08:11:35 +0330

    refactor changed configuration yo yaml

--- src/main/java/ir/dehghani/gamification/GamificationApplication.java
+++ src/main/java/ir/dehghani/gamification/GamificationApplication.java
@@ -1,52 +1,27 @@
 package ir.dehghani.gamification;
 
+import ir.dehghani.gamification.config.ServerProperties;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
-import org.springframework.core.env.Environment;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
 
 @SpringBootApplication
-public class GamificationApplication {
+public class GamificationApplication implements CommandLineRunner {
+
+    @Autowired
+    private ServerProperties properties;
 
     public static void main(String[] args) {
-         new SpringApplicationBuilder(GamificationApplication.class)
-                 .profiles("dev", "prod")
-                .run(args);
+        SpringApplication.run(GamificationApplication.class, args);
     }
 
-
-    @Component
-    class MyRunner implements CommandLineRunner {
-
-        @Autowired
-        private Environment environment;
-
-        @Override
-        public void run(String... args) throws Exception {
-            System.out.println("Active profiles: " +
-                    Arrays.toString(environment.getActiveProfiles()));
-        }
+    public void run(String... args) throws Exception {
+        System.out.println("using environment: " + properties.getEnvironment());
+        System.out.println("name: " + properties.getName());
+        System.out.println("enabled:" + properties.isEnabled());
+        System.out.println("servers: " + properties.getServers());
     }
 
-    @Component
-    @Profile(value = {"dev", "prod"})
-    class MyRunner1 implements CommandLineRunner {
-
-        @Value("${message}")
-        private String message;
-
-        @Override
-        public void run(String... args) throws Exception {
-
-            System.out.println("Message: " + message);
-        }
-    }
 
 }
---
+++ src/main/java/ir/dehghani/gamification/config/ServerProperties.java
@@ -0,0 +1,24 @@
+package ir.dehghani.gamification.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties
+@Getter
+@Setter
+public class ServerProperties {
+
+    private String name;
+    private String environment;
+    private boolean enabled;
+    private List<String> servers = new ArrayList<>();
+
+}
