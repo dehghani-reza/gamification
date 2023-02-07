ec5c400d235f620466575d70bb349ce75e92906b
dehghani <dehghan7447@gmail.com>
2023-02-07 23:22:32 +0330

    chore(core): add app-metadata to yml

--- core/src/main/java/com/gamification/core/core/ServerProperties.java
+++ core/src/main/java/com/gamification/core/core/ServerProperties.java
@@ -6,19 +6,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Configuration
 @EnableConfigurationProperties
-@ConfigurationProperties
+@ConfigurationProperties(prefix = "app-metadata")
 @Getter
 @Setter
 public class ServerProperties {
-
-    private String name;
     private String environment;
-    private boolean enabled;
-    private List<String> servers = new ArrayList<>();
-
 }
