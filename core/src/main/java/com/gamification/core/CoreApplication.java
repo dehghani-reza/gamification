5c7d2f52a4c333b222f0e85a3359273d3c54734e
Tooling <nrdc@example.com>
2023-02-07 09:49:03 +0330

    trim .gitignore and delete not used files

--- core/src/main/java/com/gamification/core/CoreApplication.java
+++ core/src/main/java/com/gamification/core/CoreApplication.java
@@ -8,7 +8,6 @@ public class CoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CoreApplication.class, args);
-        System.out.println("Hi");
     }
 
 }
