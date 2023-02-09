d161eaa0a5c01a40f63a7639445491bea699445f
dehghani <dehghan7447@gmail.com>
2023-02-09 17:31:10 +0330

    refactor(BaseEntityDTO): create base entity dto class

--- core/src/main/java/com/gamification/core/model/BaseDTO.java
+++ core/src/main/java/com/gamification/core/model/BaseDTO.java
@@ -1,15 +1,15 @@
 package com.gamification.core.model;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
-
-@Data@NoArgsConstructor
+import com.gamification.core.enumeration.EN_STATE;
+import lombok.*;
+@Data
+@NoArgsConstructor
 public abstract class BaseDTO {
 
     private Long id;
-    private Short version;
-    private LocalDateTime createdDate;
-    private LocalDateTime updatedDate;
+    private String ip;
+    private Long version;
+    private EN_STATE state;
+    private Integer rowLevelId;
+    private Integer priority;
 }
