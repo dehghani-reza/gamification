f8ed8e0f2e793f8da056e6d663ecbac691768419
Mohammad Reza Dehghani <58255161+dehghani-reza@users.noreply.github.com>
2023-04-12 11:43:12 +0330

    Chore/change input dto class (#35)
    * chore(base-service): rename generic name to convention
    
    * chore(mapper): use map struct to save end point
    
    * chore(mapper): implement save endpoint based on mapstruct
    
    * chore(base-controller): add crud end point to base controller
    
    * feat(gamification): add postgres database (#27)
    
    * feat(gamification): add postgres database
    
    * refactor(gamification): remove some data about H2 data base
    
    * chore(mapper): use map struct instead customize mapper
    
    * chore(base-service): rename generic name to convention
    
    * chore(mapper): use map struct to save end point
    
    * chore(mapper): implement save endpoint based on mapstruct
    
    * feat(gamification): add postgres database
    
    ---------
    
    Co-authored-by: AMIR FATHI <fathiamir37@gmail.com>
    
    * Chore/change user entity  (#31)
    
    * chore(gamification):
    change user entity that compatible to spring security
    
    * refactor(user-entity): change user entity that compatible to spring security
    add field to user entity-change dependency on pom file
    
    * chore: merge to development
    
    * chore(mapper): implement save endpoint based on mapstruct
    
    * chore(base-mapper): recover base mapper
    
    * chore(yml): disable auto config spring security
    
    * chore(input-dto): add fields to BaseDto class
    
    ---------
    
    Co-authored-by: AMIR FATHI <fathiamir37@gmail.com>
    Co-authored-by: Danial <122531782+DanialMehrabi94@users.noreply.github.com>

--- core/src/main/java/com/gamification/core/model/InputDTO.java
+++ core/src/main/java/com/gamification/core/model/InputDTO.java
@@ -1,9 +1,17 @@
 package com.gamification.core.model;
 
+import com.gamification.core.enumeration.EN_ACTION_TYPE;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class InputDTO<D extends BaseDTO> {
+    private EN_ACTION_TYPE actionType;
+    private Long fieldId;
+    private String hashCode;
     private Long userId;
+    private String session;
     private D data;
+    private List<InputFilter> inputFilterList;
 }
