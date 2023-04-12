b9a95476f29d46e85ba365cc00712f908f5b2da3
dehghani <dehghan7447@gmail.com>
2023-04-12 11:31:30 +0330

    chore(swagger): change uri and domain name

--- core/src/main/java/com/gamification/core/Swagger2Configuration.java
+++ core/src/main/java/com/gamification/core/Swagger2Configuration.java
@@ -2,9 +2,6 @@ package com.gamification.core;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.w3c.dom.DocumentType;
-import springfox.documentation.RequestHandler;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -12,7 +9,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Collection;
 import java.util.Collections;
 
 @Configuration
@@ -23,24 +19,22 @@ public class Swagger2Configuration {
     public Docket swaggerConfig() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-//                .paths(PathSelectors.ant("/person/*"))
                 .apis(RequestHandlerSelectors.basePackage("com.gamification.core.controllers"))
                 .build()
                 .apiInfo(apiInfoDetails());
     }
 
     private ApiInfo apiInfoDetails() {
-        ApiInfo apiInfo = new ApiInfo("gamification",
+        return new ApiInfo("gamification",
                 "This is a feature of scrume methology APIs",
                 "1.0",
                 "127.0.0.1:9092",
-                new Contact("gami fication",
-                        "http:\\gamification2023.ir",
-                        "gamification2023@gmail.com"),
+                new Contact("smartagilify",
+                        "http:\\smartagilify.com",
+                        "smartagilify@gmail.com"),
                 "API licence",
-                "http:\\gamification2023.ir",
+                "http:\\smartagilify.ir",
                 Collections.emptyList());
-        return apiInfo;
     }
 
 }
