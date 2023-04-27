deebe04a3553e4615ef7a99aeabc70ca5118b6ec
amirfathi37 <fathiamir37@gmail.com>
2023-04-27 18:52:25 +0430

    chore(person): add constructor and unimmutable

--- src/main/java/com/smartagilify/core/model/person/PersonDTO.java
+++ src/main/java/com/smartagilify/core/model/person/PersonDTO.java
@@ -3,10 +3,13 @@ package com.smartagilify.core.model.person;
 import com.smartagilify.core.model.BaseDTO;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
-@Value
 @ApiModel(description = "Details about person")
+@AllArgsConstructor
+@NoArgsConstructor
 public class PersonDTO extends BaseDTO {
     @ApiModelProperty(notes = "this is first name of person")
     private String firstName;
